fe8eadc0c48ff7293598391aafe1f27d0e2d0cc7
felix romero <firomero@uci.cu>
2014-06-22 18:20:12 -0400

    Inicalizando el framework

--- autoload.php
+++ autoload.php
@@ -3,5 +3,14 @@ require_once __DIR__.'/vendor/symfony/symfony/src/Symfony/Component/ClassLoader/
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 $loader = new UniversalClassLoader();
 $loader->register();
+if (isset($_GET['name']))
+{
+  $input = $_GET['name'];
+ 
+printf('Hello %s', $_GET['name']);
+	
+}
+
+
 
 
