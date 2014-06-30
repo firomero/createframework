5b43a352a9eeb0908e88660979dfa1116dfa9389
felix <firomero@uci.cu>
2014-06-30 11:49:58 -0400

    Capítulo 2 - HTTPFoundation

--- autoload.php
+++ autoload.php
@@ -1,15 +1,9 @@
 <?php
-require_once __DIR__.'/vendor/symfony/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__.'/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 $loader = new UniversalClassLoader();
 $loader->register();
-if (isset($_GET['name']))
-{
-  $input = $_GET['name'];
- 
-printf('Hello %s', $_GET['name']);
-	
-}
+$loader->registerNamespace('Symfony\\Component\\HttpFoundation', __DIR__.'/vendor/symfony/http-foundation');
 
 
 
