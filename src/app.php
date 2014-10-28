d9bdf9ca524e0f4a45c68436f2489b34ad4bad08
felix romero <firomero@uci.cu>
2014-10-27 21:31:30 -0400

    Capítulo 5

--- src/app.php
+++ src/app.php
@@ -1,8 +1,25 @@
 <?php
 use Symfony\Component\Routing;
- 
+use Symfony\Component\HttpFoundation\Response;
+
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
+}
+
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Routing\Route('/bye'));
+$routes->add('es_bisiesto', new Routing\Route('/es_bisiesto/{year}', array(
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response(utf8_decode('¡Sí, es bisiesto!'));
+        }
+
+        return new Response('No no lo es.');
+    }
+)));
  
 return $routes;
