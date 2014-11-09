86a84340cb3be1097f29a25014013373645f77d7
felix romero <firomero@uci.cu>
2014-11-09 16:56:20 -0500

    Capítulo 6 Carga bajo demanda de controladores

--- src/app.php
+++ src/app.php
@@ -1,6 +1,7 @@
 <?php
 use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 function is_leap_year($year = null) {
     if (null === $year) {
@@ -10,16 +11,26 @@ function is_leap_year($year = null) {
     return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
 }
 
+class LeapYearController
+{
+    /**
+     * @param $request
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('¡Sí, es bisiesto!');
+        }
+ 
+        return new Response('No no lo es.');
+    }
+}
+
 $routes = new Routing\RouteCollection();
 $routes->add('es_bisiesto', new Routing\Route('/es_bisiesto/{year}', array(
     'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response(utf8_decode('¡Sí, es bisiesto!'));
-        }
-
-        return new Response('No no lo es.');
-    }
+    '_controller' => 'LeapYearController::indexAction',
 )));
  
 return $routes;
--- web/front.php
+++ web/front.php
@@ -5,11 +5,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
-
+use Symfony\Component\HttpKernel;
 
 function render_template($request)
 {
-    extract($request->attributes->all(), EXTR_SKIP);
+    extract($request->attributes->all());
     ob_start();
     include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
 
@@ -23,14 +23,22 @@ $routes = include __DIR__.'/../src/app.php';
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$resolver = new HttpKernel\Controller\ControllerResolver();
 
-try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-    $response = call_user_func($request->attributes->get('_controller'), $request);
-} catch (Routing\Exception\ResourceNotFoundException $e) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $e) {
-    $response = new Response('An error occurred', 500);
+try 
+{
+    $request->attributes->add($matcher->match($request->getPathInfo()));   
+    $controller = $resolver->getController($request);    
+    $arguments = $resolver->getArguments($request, $controller);
+    $response = call_user_func_array($controller, $arguments);
+} 
+catch (Routing\Exception\ResourceNotFoundException $e) 
+{
+    $response = new Response('No Encontrado', 404);
+} catch (Exception $e) 
+{
+	
+    $response = new Response('Ha ocurrido un error: '.$e->getMessage(), 500);
 }
 
  
