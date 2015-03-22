561f3845feea9d8da290d1ad75dca3abfae66082
felix romero <firomero@uci.cu>
2015-03-22 10:57:25 -0400

    Capítulo 12

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -1,51 +1,14 @@
 <?php
 
 namespace Simplex;
- 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+
+use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
 use Symfony\Component\EventDispatcher\EventDispatcher;
  
-class Framework implements HttpKernelInterface
+class Framework extends HttpKernel\HttpKernel
 {
-    protected $matcher;
-    protected $resolver;
- 
-    public function __construct(EventDispatcher $dispatcher,UrlMatcher $matcher, ControllerResolver $resolver)
-    {
-        $this->matcher = $matcher;
-        $this->resolver = $resolver;
-        $this->dispatcher = $dispatcher;
-    }
 
-    /**
-     * @param Request $request
-     * @param int $type
-     * @param bool $catch
-     * @return mixed|Response
-     */
-    public function handle(Request $request,$type = self::MASTER_REQUEST, $catch = true)
-    {
-        try {
-            $request->attributes->add($this->matcher->match($request->getPathInfo()));
- 
-            $controller = $this->resolver->getController($request);
-            $arguments = $this->resolver->getArguments($request, $controller);
 
-            $response = call_user_func_array($controller, $arguments);
 
-        } catch (ResourceNotFoundException $e) {
-            return new Response('Not Found', 404);
-        } catch (\Exception $e) {
-            return new Response('An error occurred', 500);
-        }
-        // dispatch a response event
-        $this->dispatcher->dispatch('response', new ResponseEvent($response, $request));
-
-        return $response;
-    }
 }
--- web/front.php
+++ web/front.php
@@ -1,56 +1,18 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
- 
+
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-
-function render_template($request)
-{
-    extract($request->attributes->all());
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
-
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
- 
-$context = new Routing\RequestContext();
-$context->fromRequest($request);
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$resolver = new HttpKernel\Controller\ControllerResolver();
-$dispatcher = new EventDispatcher();
+$sc = include __DIR__.'/../src/container.php';
 
-$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
-$dispatcher->addSubscriber(new Simplex\GoogleListener());
+$request = Request::createFromGlobals();
 
-$dispatcher->addListener('response', function (Simplex\ResponseEvent $event) {
-    $response = $event->getResponse();
-    $headers = $response->headers;
+/**
+ * @var \Symfony\Component\HttpFoundation\Response $response
+ */
+$response = $sc->get('framework')->handle($request);
 
-    if (!$headers->has('Content-Length') && !$headers->has('Transfer-Encoding')) {
-        $headers->set('Content-Length', strlen($response->getContent()));
-    }
-}, -255);
-
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
-$errorHandler = (function (HttpKernel\Exception\FlattenException $exception) {
-    $msg = 'Something went wrong! ('.$exception->getMessage().')';
-
-    return new Response($msg, $exception->getStatusCode());
-});
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ExceptionListener($errorHandler));
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
-
-$framework = new Simplex\Framework($dispatcher, $matcher, $resolver);
-$framework = new HttpCache($framework, new Store(__DIR__.'/../cache'));
-$response = $framework->handle($request);
- 
 $response->send();
